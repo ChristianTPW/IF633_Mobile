a115b3c4594e649404ab800001c52b000b294363
ChristianTPW <49968741+ChristianTPW@users.noreply.github.com>
2021-01-26 11:16:02 +0700

    Week 01

---
+++ IF633_DL/Week01_32062/app/src/main/java/umn/ac/week01_32062/MainActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.week01_32062;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
