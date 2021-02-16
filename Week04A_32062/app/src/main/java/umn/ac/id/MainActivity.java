518cce91364c53586202cf1f50498d4eb6b4404b
Christian Widjaja <christianwidjaja04@gmail.com>
2021-02-16 11:04:06 +0700

    Week 4
    Browse intent bug fix

--- Week04A_32062/app/src/main/java/umn/ac/id/MainActivity.java
+++ Week04A_32062/app/src/main/java/umn/ac/id/MainActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 Intent browseIntent = new Intent(Intent.ACTION_VIEW);
                 browseIntent.setData(Uri.parse(urlText));
+
                 if (browseIntent.resolveActivity(getPackageManager())!= null){
                     startActivity(browseIntent);
                 }
